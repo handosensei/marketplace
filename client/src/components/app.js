4d2e335d4db4b09ce907962dfc3970a16fdf76db
Hando Masahashi <handosensei@gmail.com>
2022-05-16 01:03:40 +0200

    useless

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -3,33 +3,7 @@ import { Router, Location, Redirect } from '@reach/router';
 import ScrollToTopBtn from './menu/ScrollToTop';
 import Header from './menu/header';
 import Home from './pages/home';
-import Home1 from './pages/home1';
-import Home2 from './pages/home2';
-import Explore from './pages/explore';
-import Explore2 from './pages/explore2';
-import Helpcenter from './pages/helpcenter';
-import Rangking from './pages/rangking';
-import Colection from './pages/colection';
-import ItemDetail from './pages/ItemDetail';
-import Author from './pages/Author';
-import Wallet from './pages/wallet';
-import Login from './pages/login';
-import LoginTwo from './pages/loginTwo';
-import Register from './pages/register';
-import Price from './pages/price';
-import Works from './pages/works';
-import News from './pages/news';
-import Create from './pages/create';
-import Auction from './pages/Auction';
-import Activity from './pages/activity';
-import Contact from './pages/contact';
-import ElegantIcons from './pages/elegantIcons';
-import EtlineIcons from './pages/etlineIcons';
-import FontAwesomeIcons from './pages/fontAwesomeIcons';
-import Accordion from './pages/accordion';
-import Alerts from './pages/alerts';
-import Progressbar from './pages/progressbar';
-import Tabs from './pages/tabs';
+
 
 import { NotificationContainer } from 'react-notifications';
 
@@ -85,34 +59,7 @@ const app= () => (
           <Home exact path="/">
           <Redirect to="/home" />
         </Home>
-        <Home1 path="/home1" />
-        <Home2 path="/home2" />
-        <Explore path="/explore" />
-        <Explore2 path="/explore2" />
-        <Helpcenter path="/helpcenter" />
-        <Rangking path="/rangking" />
-        <Colection path="/colection" />
-        <ItemDetail path="/ItemDetail" />
-        <Author path="/Author" />
-        <Wallet path="/wallet" />
-        <Login path="/login" />
-        <LoginTwo path="/loginTwo" />
-        <Register path="/register" />
-        <Price path="/price" />
-        <Works path="/works" />
-        <News path="/news" />
-        <Create path="/create" />
-        <Auction path="/Auction" />
-        <Activity path="/activity" />
-        <Contact path="/contact" />
-        <ElegantIcons path="/elegantIcons" />
-        <EtlineIcons path="/etlineIcons" />
-        <FontAwesomeIcons path="/fontAwesomeIcons" />
-        <Accordion path="/accordion" />
-        <Alerts path="/alerts" />
-        <Progressbar path="/progressbar" />
-        <Tabs path="/tabs" />
-
+        
         <ERC721FactoryCreate  path="/erc721/collections/create" />
         <ERC721Display        path="/erc721/:collectionAddress/:tokenId" />
         <ERC721Create         path="/erc721/:collectionAddress/create" />
--- client/src/components/menu/header.js
+++ client/src/components/menu/header.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Breakpoint, { BreakpointProvider, setDefaultBreakpoints } from "react-socks";
 import { Link } from '@reach/router';
-import useOnclickOutside from "react-cool-onclickoutside";
 
 setDefaultBreakpoints([
   { xs: 0 },
@@ -25,10 +24,7 @@ const NavLink = props => (
 const Header= function() {
 
     const [userAddress, setUserAddress] = React.useState("");
-    const [openMenu1, setOpenMenu1] = React.useState(false);
-    const [openMenu2, setOpenMenu2] = React.useState(false);
-    const [openMenu3, setOpenMenu3] = React.useState(false);
-    const [showmenu, btn_icon] = useState(false);
+    const [, btn_icon] = useState(false);
 
     async function connect(onConnected) {
       if (!window.ethereum) {
@@ -95,34 +91,6 @@ const Header= function() {
         );
     }
   
-    const handleBtnClick1 = () => {
-      setOpenMenu1(!openMenu1);
-    };
-    const handleBtnClick2 = () => {
-      setOpenMenu2(!openMenu2);
-    };
-    const handleBtnClick3 = () => {
-      setOpenMenu3(!openMenu3);
-    };
-    const closeMenu1 = () => {
-      setOpenMenu1(false);
-    };
-    const closeMenu2 = () => {
-      setOpenMenu2(false);
-    };
-    const closeMenu3 = () => {
-      setOpenMenu3(false);
-    };
-    const ref1 = useOnclickOutside(() => {
-      closeMenu1();
-    });
-    const ref2 = useOnclickOutside(() => {
-      closeMenu2();
-    });
-    const ref3 = useOnclickOutside(() => {
-      closeMenu3();
-    });
-
     useEffect(() => {
 
       const header = document.getElementById("myHeader");
@@ -138,7 +106,6 @@ const Header= function() {
         header.classList.remove("sticky");
         totop.classList.remove("show");
       } 
-      
       });
   
       checkIfWalletIsConnected(setUserAddress);    
@@ -166,77 +133,6 @@ const Header= function() {
                   
                   <UserConnectLink />
 
-                  <div className='navbar-item'>
-                    <div ref={ref1}>
-                        <div className="dropdown-custom dropdown-toggle btn" 
-                          onMouseEnter={handleBtnClick1} onMouseLeave={closeMenu1}>
-                          Explore
-                          <span className='lines'></span>
-                          {openMenu1 && (
-                          <div className='item-dropdown'>
-                            <div className="dropdown" onClick={closeMenu1}>
-                            <NavLink to="/explore">Explore</NavLink>
-                            <NavLink to="/explore2">Explore 2</NavLink>
-                            <NavLink to="/rangking">Rangking</NavLink>
-                            <NavLink to="/colection">Collection</NavLink>
-                            <NavLink to="/ItemDetail">Items Details</NavLink>
-                            <NavLink to="/Auction">Live Auction</NavLink>
-                            <NavLink to="/helpcenter">Help Center</NavLink>
-                            </div>
-                          </div>
-                        )}
-                        </div>
-                        
-                      </div>
-                  </div>
-                  <div className='navbar-item'>
-                    <div ref={ref2}>
-                        <div className="dropdown-custom dropdown-toggle btn" 
-                          onMouseEnter={handleBtnClick2} onMouseLeave={closeMenu2}>
-                          Pages
-                          <span className='lines'></span>
-                          {openMenu2 && (
-                          <div className='item-dropdown'>
-                            <div className="dropdown" onClick={closeMenu2}>
-                            <NavLink to="/Author">Author</NavLink>
-                            <NavLink to="/wallet">Wallet</NavLink>
-                            <NavLink to="/create">Create</NavLink>
-                            <NavLink to="/news">News</NavLink>
-                            <NavLink to="/works">Gallery</NavLink>
-                            <NavLink to="/login">login</NavLink>
-                            <NavLink to="/loginTwo">login 2</NavLink>
-                            <NavLink to="/register">Register</NavLink>
-                            <NavLink to="/contact">Contact Us</NavLink>
-                            </div>
-                          </div>
-                        )}
-                        </div>
-                      </div>
-                  </div>
-                  
-                  <div className='navbar-item'>
-                    <div ref={ref3}>
-                        <div className="dropdown-custom dropdown-toggle btn" 
-                          onMouseEnter={handleBtnClick3} onMouseLeave={closeMenu3}>
-                          Elements
-                          <span className='lines'></span>
-                          {openMenu3 && (
-                          <div className='item-dropdown'>
-                            <div className="dropdown" onClick={closeMenu3}>
-                            <NavLink to="/elegantIcons">Elegant Icon</NavLink>
-                            <NavLink to="/etlineIcons">Etline Icon</NavLink>
-                            <NavLink to="/fontAwesomeIcons">Font Awesome Icon</NavLink>
-                            <NavLink to="/accordion">Accordion</NavLink>
-                            <NavLink to="/alerts">Alerts</NavLink>
-                            <NavLink to="/price">Pricing Table</NavLink>
-                            <NavLink to="/progressbar">Progess Bar</NavLink>
-                            <NavLink to="/tabs">Tabs</NavLink>
-                            </div>
-                          </div>
-                        )}
-                        </div>
-                      </div>
-                  </div>
                 </div>
               </Breakpoint>
             </BreakpointProvider>
@@ -245,12 +141,6 @@ const Header= function() {
               { userAddress ? (<Address userAddress={userAddress} />) : (<Connect setUserAddress={setUserAddress}/>)}
             </div>
           </div>
-
-          <button className="nav-icon" onClick={() => btn_icon(!showmenu)}>
-            <div className="menu-line white"></div>
-            <div className="menu-line1 white"></div>
-            <div className="menu-line2 white"></div>
-          </button>
 
         </div>     
       </header>
